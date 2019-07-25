fafac34d33224b39ef7923a8d00c9e411fe9d823
aspak <sharifaspak@gmail.com>
2019-07-25 22:09:41 +0530

    gb commit

--- bbbb/src/main/java/aaaa/bbbb/App.java
+++ bbbb/src/main/java/aaaa/bbbb/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello GIT World!" );
+        System.out.println( "Hello GIT  bash World!" );
     }
 }
