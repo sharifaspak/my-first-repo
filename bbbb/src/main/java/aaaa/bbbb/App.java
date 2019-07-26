1d5458f410e0907013676f68b66a4909e16c30fa
aspak <sharifaspak@gmail.com>
2019-07-26 21:51:27 +0530

    checking reset

--- bbbb/src/main/java/aaaa/bbbb/App.java
+++ bbbb/src/main/java/aaaa/bbbb/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Aspak is changing main branch" );
+        System.out.println( "Aspak is resetting  main branch" );
     }
 }
